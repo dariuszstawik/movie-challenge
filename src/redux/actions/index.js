78a14bdb5485c8b2ade0e08897f9c6d90f5b79a9
Dariusz Stawik <dariusz.stawik@gmail.com>
2023-03-10 17:08:21 +0100

    Add store, actions and reducer in redux

---
+++ src/redux/actions/index.js
@@ -0,0 +1,43 @@
+import { actionTypes } from "./actionTypes";
+
+export const setSecondsAmount = (payload) => {
+  return {
+    type: actionTypes.SET_SECONDS_AMOUNT,
+    payload,
+  };
+};
+
+export const selectMovie = (payload) => {
+  return {
+    type: actionTypes.SELECT_MOVIE,
+    payload,
+  };
+};
+
+export const chooseRandomNumbers = (payload) => {
+  return {
+    type: actionTypes.CHOOSE_RANDOM_NUMBERS,
+    payload,
+  };
+};
+
+export const setResults = (payload) => {
+  return {
+    type: actionTypes.SET_RESULTS,
+    payload,
+  };
+};
+
+export const setIsResultVisible = (payload) => {
+  return {
+    type: actionTypes.SET_IS_RESULT_VISIBLE,
+    payload,
+  };
+};
+
+export const setTimeRange = (payload) => {
+  return {
+    type: actionTypes.SET_TIME_RANGE,
+    payload,
+  };
+};
--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -1,3 +1,5 @@
+import { actionTypes } from "../actions/actionTypes";
+
 const initialState = {
   secondsAmount: 0,
   selectedMovie: 2,
@@ -11,40 +13,36 @@ const rootReducer = (state = initialState, action) => {
   const { type, payload } = action;
 
   switch (type) {
-    case "SET_SECONDS_AMOUNT":
+    case actionTypes.SET_SECONDS_AMOUNT:
       return {
         ...state,
         secondsAmount: payload,
       };
 
-    case "SELECT_MOVIE":
+    case actionTypes.SELECT_MOVIE:
       return {
         ...state,
         selectedMovie: payload,
       };
-    case "CHOOSE_RANDOM_NUMBERS":
+
+    case actionTypes.CHOOSE_RANDOM_NUMBERS:
       return {
         ...state,
         randomNumbers: payload,
       };
 
-    case "SET_RESULTS":
+    case actionTypes.SET_RESULTS:
       return {
         ...state,
         results: [...results, payload],
       };
 
-    case "SET_RESULTS":
-      return {
-        ...state,
-        results: [...results, payload],
-      };
     case "SET_IS_RESULT_VISIBLE":
       return {
         ...state,
         isResultVisible: payload,
       };
-    case "SET_TIMING":
+    case "SET_TIME_RANGE":
       return {
         ...state,
         timing: payload,
