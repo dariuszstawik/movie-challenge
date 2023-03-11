57b15c809240a187ce617a2266f69a10d68e372a
Dariusz Stawik <dariusz.stawik@gmail.com>
2023-03-11 22:14:09 +0100

    Add basic movies data

---
+++ src/movies-data/moviesData.js
@@ -0,0 +1,193 @@
+export const moviesData = [
+  {
+    id: "1",
+    title: "Kogel-Mogel",
+    youtubeId: "DSLvLY8yTMw",
+  },
+  {
+    id: "2",
+    title: "Awantura o Basię",
+    youtubeId: "pdAP9WI3nSg",
+  },
+  {
+    id: "3",
+    title: "Wyjście awaryjne",
+    youtubeId: "4Ou0IAtcsJI",
+  },
+  {
+    id: "4",
+    title: "Pieniądze to nie wszystko",
+    youtubeId: "alpedk2DYI0",
+    // trailer: uMWlUykpIW4
+  },
+  {
+    id: "5",
+    title: "Jak rozpętałem Drugą Wojnę Światową",
+    youtubeId: "KccUweDVTEg",
+  },
+  {
+    id: "6",
+    title: "Co mi zrobiszjak mnie złapiesz",
+    youtubeId: "KIr8DE8-SFI",
+  },
+  {
+    id: "7",
+    title: "Rozmowy kontrolowane",
+    youtubeId: "hidzrLNupqs",
+  },
+  {
+    id: "8",
+    title: "Niespotykanie spokojny człowiek",
+    youtubeId: "khCdIUIrauU",
+  },
+  {
+    id: "9",
+    title: "Miś",
+    youtubeId: "rEcEnRDLAew",
+  },
+  {
+    id: "10",
+    title: "Poszukiwany, poszukiwana",
+    youtubeId: "NlkhxZAX2Ho",
+  },
+  {
+    id: "11",
+    title: "Nie ma mocnych",
+    youtubeId: "xmBuWoP4cwY",
+  },
+  {
+    id: "12",
+    title: "Daleko od szosy",
+    youtubeId: "M_B5ci8VlMs",
+  },
+  {
+    id: "13",
+    title: "W pustyni i w puszczy",
+    youtubeId: "JGVd8O3qM5g",
+  },
+  {
+    id: "14",
+    title: "Brunet wieczorową porą",
+    youtubeId: "f5lZE-MnnYs",
+  },
+  {
+    id: "15",
+    title: "Faraon",
+    youtubeId: "4wR7UtfoGIw",
+  },
+  {
+    id: "16",
+    title: "Nóż w wodzie",
+    youtubeId: "95aXQ2XtV3E",
+  },
+  {
+    id: "17",
+    title: "Janosik",
+    youtubeId: "QpTAMvbkKHE",
+  },
+  {
+    id: "18",
+    title: "Nie lubię poniedziałku",
+    youtubeId: "1K7Nn0AxOlI",
+  },
+  {
+    id: "19",
+    title: "Kulfon i Monika",
+    youtubeId: "K_4G25ylTUY",
+  },
+  {
+    id: "20",
+    title: "Chłopaki nie płaczą",
+    youtubeId: "SzlOnRYQW_w",
+  },
+  {
+    id: "21",
+    title: "07 zgłoś się",
+    youtubeId: "5jtUe3Gk9YE",
+  },
+  {
+    id: "22",
+    title: "Dom",
+    youtubeId: "bOUbNCPM3sg",
+  },
+  {
+    id: "23",
+    title: "Stawka większa niż życie",
+    youtubeId: "Wx7v_u7a-eE",
+  },
+  {
+    id: "24",
+    title: "Czterej pancerni i pies",
+    youtubeId: "tr84QJ3BmGw",
+  },
+  {
+    id: "25",
+    title: "Znachor",
+    youtubeId: "4mO800K_i1w",
+  },
+  {
+    id: "26",
+    title: "Ranczo Wilkowyje",
+    youtubeId: "UIEo_FdnvO4",
+  },
+  {
+    id: "27",
+    title: "Alternatywy 4",
+    youtubeId: "WiV2N8k2ViI",
+  },
+  {
+    id: "28",
+    title: "Czterdziestolatek",
+    youtubeId: "KnDFsf6FgTs",
+  },
+  {
+    id: "29",
+    title: "Krzyżacy",
+    youtubeId: "TfhJvKFc2E0",
+  },
+  {
+    id: "30",
+    title: "Sprawa się rypła",
+    youtubeId: "FkJ2a6WXXTM",
+  },
+  {
+    id: "31",
+    title: "Piłkarski poker",
+    youtubeId: "iWeOlVQzDnQ",
+  },
+  {
+    id: "32",
+    title: "Dzień Świra",
+    youtubeId: "MmQqpEy96r4",
+  },
+  {
+    id: "33",
+    title: "Zezowate szczęśćie",
+    youtubeId: "aR1PPwyuUcM",
+  },
+  {
+    id: "34",
+    title: "Zmiennicy",
+    youtubeId: "e90_jWsI0Uw",
+  },
+  {
+    id: "35",
+    title: "Święta wojna",
+    youtubeId: "M__HJLAHCqE",
+  },
+  {
+    id: "36",
+    title: "Quo Vadis",
+    youtubeId: "rwxTB3z-ZDE",
+  },
+  {
+    id: "37",
+    title: "Przepraszam, czy tu biją?",
+    youtubeId: "xyyTw3ezdas",
+  },
+  {
+    id: "38",
+    title: "Wiosna, panie sierżancie",
+    youtubeId: "QqHLbLjxgkM",
+  },
+];
